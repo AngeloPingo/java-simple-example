2d44cdfe14c29554567839b88d089bf2336a1624
Ângelo Pingo <angelo.pingo@celfocus.com>
2020-03-12 14:46:29 +0000

    Initial commit.

---
+++ src/main/java/pt/angelopingo/examples/MainApp.java
@@ -0,0 +1,13 @@
+package pt.angelopingo.examples;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainApp {
+
+    public static void main(String[] args) {
+        log.info("Application started.");
+        System.out.println("Hello World.");
+    }
+
+}
